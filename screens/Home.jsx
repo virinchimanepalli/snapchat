2ac30c76bb2003e10a52e87b675e4b00babbe0fd
virinchy25 <virinchi@sparkstudio.co>
2022-02-05 17:47:18 +0530

    initial commit

---
+++ screens/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from "react-native";
+
+export const Home = () => {
+  return <View>Hello world</View>;
+};
